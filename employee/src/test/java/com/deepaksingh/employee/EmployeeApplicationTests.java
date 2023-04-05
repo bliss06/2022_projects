bf29b13b1c31a9ebd2a749be7707888607260379
bliss06 <85256374+bliss06@users.noreply.github.com>
2023-04-05 15:47:54 +0530

    Add files via upload

---
+++ employee/src/test/java/com/deepaksingh/employee/EmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepaksingh.employee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
